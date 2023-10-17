6657220b4ba6c337aaef2f36010d41ec0a865393
Ronnachate <ronnachate@gmail.com>
2023-10-17 21:23:51 +0700

    Add user status seed.

---
+++ migrations/1697551344323-UserStatusSeed.ts
@@ -0,0 +1,37 @@
+import { UserStatus } from "../src/user/entities/user.status.entity";
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class UserStatusSeed1697551344323 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        let statuses: UserStatus[] = [];
+        var statusArray = [
+            {
+                id : 1,
+                name: "Active"
+            },
+            {
+                id : 2,
+                name: "Inactive"
+            },
+            {
+                id : 3,
+                name: "Deleted"
+            }
+        ];
+        for(const data of statusArray) {
+            statuses.push(queryRunner.manager.create<UserStatus>(UserStatus, {
+                id: data.id,
+                name: data.name,
+              }));
+        }
+        const createdStatused = await queryRunner.manager.save(
+            statuses
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DELETE * FROM user_statuses`);
+    }
+
+}
