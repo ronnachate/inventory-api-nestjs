f8e02282d75c720006926ad99de24951c2027968
Ronnachate <ronnachate@gmail.com>
2023-10-18 17:07:51 +0700

    Add try catch to return custom error message and log errors.

--- src/shared/constant/generic.ts
+++ src/shared/constant/generic.ts
@@ -1,3 +1,5 @@
 export const APPLICATION_KEY_HEADER = 'application_key';
 
 export const USER_DELETED_STATUS = 3;
+
+export const INTERNAL_SERVER_ERROR_MSG = 'Server error, please contact developer';
--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { LoggerService } from 'src/shared/logger/logger.service';
 import { UserService } from '../service/user/user.service';
 import { UserDTO } from '../dtos/user.dto';
 import { UserPaginationParams } from '../query-params/pagination-params';
 import { PaginationResultset } from 'src/shared/dtos/pagination-resultset';
+import { INTERNAL_SERVER_ERROR_MSG } from 'src/shared/constant/generic';
 
 @Controller('users')
 export class UserController {
@@ -18,15 +26,47 @@ export class UserController {
   async getUsers(
     @Query() query: UserPaginationParams
   ): Promise<PaginationResultset<UserDTO[]>> {
-    const { page = 1, rows = 10 } = query;
-    const { users, count } = await this.userService.getUsers(page, rows, query.status);
+    try {
+      const { page = 1, rows = 10 } = query;
+      const { users, count } = await this.userService.getUsers(
+        page,
+        rows,
+        query.status
+      );
 
-    return { items: users, pagination: { page, rows, count } };
+      return { items: users, pagination: { page, rows, count } };
+    } catch (error) {
+      this.logger.error("getUsers error with", query);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: INTERNAL_SERVER_ERROR_MSG,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: error,
+        }
+      );
+    }
   }
 
   @Get(':id')
   async getUser(@Param('id') id: number): Promise<UserDTO> {
-    const user = await this.userService.getUserById(id);
-    return user;
+    try {
+      const user = await this.userService.getUserById(id);
+      return user;
+    } catch (error) {
+      this.logger.error(`getUsers error with id "${id}"`, error);
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: INTERNAL_SERVER_ERROR_MSG,
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        {
+          cause: error,
+        }
+      );
+    }
   }
 }
