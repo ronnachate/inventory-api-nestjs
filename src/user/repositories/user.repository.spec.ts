27bc4166ef01ffe7348726a99e8c0e4c8892f1f7
Ronnachate <ronnachate@gmail.com>
2023-10-18 21:34:27 +0700

    Add unit test for repositoty.getById

--- src/user/repositories/user.repository.spec.ts
+++ src/user/repositories/user.repository.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserRepository } from './user.repository';
 import { DataSource } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { UserStatus } from '../entities/user.status.entity';
 
 describe('UserRepository', () => {
   let repository: UserRepository;
@@ -27,5 +29,37 @@ describe('UserRepository', () => {
 
   it('should be defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  describe('Get user by id', () => {
+    const currentDate = new Date();
+    const id = 1;
+
+    const expectedOutput: User = {
+      id,
+      title: 'title1',
+      name: 'User1',
+      lastname: 'lastname1',
+      username: 'user1',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+      status: null,
+      permission: null,
+    };
+
+    it('should call findOne with correct user id', () => {
+      jest
+        .spyOn(repository, 'findOne')
+        .mockImplementation(async () => expectedOutput);
+      repository.getById(id);
+      expect(repository.findOne).toHaveBeenCalledWith({ where: { id } });
+    });
+
+    it('should return correct user data', async () => {
+      jest
+        .spyOn(repository, 'findOne')
+        .mockImplementation(async () => expectedOutput);
+      expect(await repository.getById(1)).toEqual(expectedOutput);
+    });
   });
 });
