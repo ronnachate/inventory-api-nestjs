ae409bc648b1dfc60da3d75c2e579d1bd37038bf
Ronnachate <ronnachate@gmail.com>
2023-11-03 13:51:54 +0700

    Add unit test for signin controller.

--- src/auth/controller/auth.controller.spec.ts
+++ src/auth/controller/auth.controller.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { SigninDTO } from '../dtos/signin.dto';
+import { AuthTokenDTO } from '../dtos/auth-token.dto';
+import { AuthService } from '../services/auth.service';
+import { HttpException, UnauthorizedException } from '@nestjs/common';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  const mockedAuthService = {
+    signIn: jest.fn(() => null),
+  };
+  const authToken: AuthTokenDTO = {
+    accessToken: 'random_access_token',
+    refreshToken: 'random_refresh_token',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [{ provide: AuthService, useValue: mockedAuthService }],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  describe('signin', () => {
+    const signinDto: SigninDTO = {
+      username: 'john@doe.com',
+      password: '1234',
+    };
+    it('should signin user', async () => {
+      jest
+        .spyOn(mockedAuthService, 'signIn')
+        .mockImplementation(async () => authToken);
+
+      expect(await authController.signin(signinDto)).toEqual(authToken);
+    });
+
+    it('should return UnauthorizedException when signin is invalid', async () => {
+      jest
+        .spyOn(mockedAuthService, 'signIn')
+        .mockRejectedValue(new UnauthorizedException());
+
+      try {
+        await authController.signin(signinDto);
+      } catch (error) {
+        expect(error.constructor).toBe(UnauthorizedException);
+      }
+    });
   });
 });
