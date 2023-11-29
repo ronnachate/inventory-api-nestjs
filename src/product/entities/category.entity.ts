fff8449bc8445fa92c6013fcbe73bf09da4c4360
Ronnachate <ronnachate@gmail.com>
2023-11-29 15:24:10 +0700

    Add missing category prop.

--- src/product/entities/category.entity.ts
+++ src/product/entities/category.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from 'typeorm';
 
 import { Product } from './product.entity';
 
@@ -7,11 +7,21 @@ export class Category {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 100 })
+  @Unique('name', ['name'])
+  @Column({ length: 150 })
   name: string;
+
+  @Column({ length: 255 })
+  description: string;
 
   statusId: number;
 
   @OneToMany(() => Product, (product) => product.status)
   products: Product[];
+
+  @CreateDateColumn({ name: 'createdAt', nullable: true })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updatedAt', nullable: true })
+  updatedAt: Date;
 }
